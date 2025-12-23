26aa2fa8a5632f2d2ebde3fc2db0256c263bb69d
sushant440 <sushantaryal440@gmail.com>
2025-12-23 15:52:59 +0545

    day2

---
+++ Day 2/prog2.py
@@ -0,0 +1,26 @@
+# Type Conversion
+
+# x= input("x: ")
+# y= X+1   If we run this we can get error!❌
+# There are different data types in which we can convert.
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+# we can use type conversion in such cases
+# ===========================================
+# x = input("x: ")
+# y = int(x) + 1   # This is type conversion
+# print(f"x: {x}, y:{y}")
+# ============================================
+# bool
+# falsy functions
+# ""   This is notation of empty string
+# 0
+# None
+
+print(bool(0))   #In this case false is printed
+print(bool(5))  # In this case trie is printed
+
+
